a888aede40ad5b1f64cdaf4b7e7886898e8c81dd
Mariam-Abugamga <71400493+Mariam-Abugamga@users.noreply.github.com>
2020-09-30 14:20:11 -0400

    made a traffic light

--- main.ts
+++ main.ts
@@ -1,7 +1,24 @@
-let strip: neopixel.Strip = null
+// Created by; Mariam Abugamga
+// Date: Sept 30, 2020
+// This program lights up neopixels in a traffic light form
+let strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+strip.clear()
+strip.show()
+basic.showIcon(IconNames.Yes)
 basic.forever(function () {
-    strip = neopixel.create(DigitalPin.P0, 4, NeoPixelMode.RGB)
-    strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+    strip.show()
+    basic.pause(1000)
     strip.clear()
-    basic.showIcon(IconNames.Heart)
+    strip.show()
+    strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+    strip.show()
+    basic.pause(1000)
+    strip.clear()
+    strip.show()
+    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    strip.show()
+    basic.pause(1000)
+    strip.clear()
+    strip.show()
 })
